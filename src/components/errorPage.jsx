3e2cf6dc1b39115aa4e1a3d3252cd48507db27d1
sujoychakraborty93 <sujoychakraborty93@gmail.com>
2019-12-06 12:03:30 -0700

    first commit

---
+++ src/components/errorPage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class ErrorClass extends Component {
+  state = {};
+  render() {
+    return <h1>Page not found</h1>;
+  }
+}
+
+export default ErrorClass;
